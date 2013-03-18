1d44a366e596334dcee5c1ade75ddf4680659806
Yichen Yu <chouchangke525@yahoo.com.cn>
2013-03-18 18:05:34 -0500

    root change

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Mind::Application.routes.draw do
 
   resources :admins
 
-  root :to => 'sessions#login'
+  root :to => '/login'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
