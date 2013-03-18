4a87e57f12047fb226a280e65f6561f22dce2cb7
Yichen Yu <chouchangke525@yahoo.com.cn>
2013-03-18 18:40:11 -0500

    category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ActiveRecord::Base
   attr_accessible :word
   has_many :contents
   
-  
+  validates :word, :presence => true, :uniqueness => true
   before_create do |category|
      category.word=category.word.capitalize
      end 
