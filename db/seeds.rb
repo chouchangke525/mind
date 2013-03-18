a5deda892f67fb9d1ddd6af9cb72a15215203166
Yichen Yu <chouchangke525@yahoo.com.cn>
2013-03-18 18:21:35 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,6 @@
   Category.create word: "Other"
   
   Content.create user_id: 1,
-  mind: "I am so happy today.",
-  title: "The first mind.",
-  category_id: 1
+        mind: "I am so happy today.",
+        title: "The first mind.",
+        category_id: 1
