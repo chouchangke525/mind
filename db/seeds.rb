6793d8f1bf0def53a4fcc3daedd99c97ba143438
Yichen Yu <chouchangke525@yahoo.com.cn>
2013-03-18 18:20:45 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@
       
   Category.create word: "Other"
   
-  Content.create user_id: User.find_by_name("yichen525").id,
+  Content.create user_id: 1,
   mind: "I am so happy today.",
   title: "The first mind.",
-  category_id: Category.find_by_name("Other").id
+  category_id: 1
