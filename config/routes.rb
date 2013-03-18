8d58048ce6fbd04770bd3784e1d8a4fe5032e780
Yichen Yu <chouchangke525@yahoo.com.cn>
2013-03-18 17:58:51 -0500

    root and seed

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Mind::Application.routes.draw do
 
   resources :admins
 
+  root :to => 'session#login'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,20 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
  
 
   Admin.create name: "CHOUCHANGKE525",
       
       password: "chouchangke525",
       password_confirmation: "chouchangke525"
+      
+  User.create name: "yichen525",
+      password: "yichen525",
+      password_confirmation: "yichen525"
+      
+  Category.create word: "Other"
+  
+  Content.create user_id: User.find_by_name("yichen525").id
+  mind: I am so happy today.
+  title: The first mind.
+  category_id: Category.find_by_name("Other").id
